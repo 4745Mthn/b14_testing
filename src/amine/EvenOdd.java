1c36ea77a9bf529199be891bd918e3abbe2a1eed
Amine Ersu <160289484+ersuamine@users.noreply.github.com>
2024-04-25 15:26:32 -0400

    Added EvenOdd and updated it

---
+++ src/amine/EvenOdd.java
@@ -0,0 +1,21 @@
+package amine;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        int num2 = 2;
+
+        if (num % 2 == 0){
+            System.out.println(num + "is even");
+        } else{
+            System.out.println(num + " is odd");
+        }
+
+    }
+
+}
+/*
+Check if the given number is even or odd and display message even/odd
+ */
