adb116a6e2062c78dfb2b69dadbdd7ee97c327c4
kbreab desta <destakbreab@gmail.com>
2024-04-26 13:44:05 -0400

    Created a main method with my name

---
+++ src/b14/KbreabKebi.java
@@ -0,0 +1,10 @@
+package b14;
+
+public class KbreabKebi {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Kbreab Desta");
+        System.out.println("I am an SDET programm student");
+    }
+}
