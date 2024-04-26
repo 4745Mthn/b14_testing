5783897a9506e89755d9727f840f1054ba945db8
zynpc <zynpckmk8@gmail.com>
2024-04-26 12:21:24 -0400

    created sdet objects

--- src/zeynep/SdetClients.java
+++ src/zeynep/SdetClients.java
@@ -4,6 +4,15 @@ public class SdetClients {
 
     public static void main(String[] args) {
 
+        Sdet sdet1 = new Sdet("Zeynep", 34, 'F',"Python", true);
+        Sdet sdet2 = new Sdet("Dilovar", 34, 'M', "Java", true);
+        Sdet sdet3 = new Sdet("Amine", 34, 'F', "JavaScript", true);
+        Sdet sdet4 = new Sdet("Metehan", 34, 'M', "C++", true);
+        Sdet sdet5 = new Sdet("Kbreab", 34, 'M', "Java", true);
+        Sdet sdet6 = new Sdet("Ziyoda", 34, 'F', "Kotlin", true);
+
+
+
 
     }
 }
