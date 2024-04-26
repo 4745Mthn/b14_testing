c2b35b23804e4a257e92bd3d3c1db5948b87a42e
dilovar1992 <dellovar56@gmail.com>
2024-04-26 01:12:53 -0400

    created instance variables

--- src/dilovar/Turaev.java
+++ src/dilovar/Turaev.java
@@ -2,4 +2,31 @@ package dilovar;
 
 public class Turaev {
     //dilovar turaev
+    public String name;
+    public int age;
+    public char gender;
+    public String programmingLanguages;
+     public boolean isFullStackTester;
+    /*
+Create a custom class named sdet with fallowing specifications:
+
+Attribites:
+
+            name (String)
+
+            age (int)
+            gender (char)
+            programmingLanguages (String)
+            isFullStackTester (boolean)
+
+Constructors:
+            Add multiple constructors to set different fields of the Sdets object.
+
+Actions:
+        work(): prints the  name of the employee concatenated with " is great Sdet"
+        toString(): returns a string representation of the sdet object.
+
+    Create another class named sdetClients, create multiple Sdets objects, and test each function of the sdet object.
+
+*/
 }
