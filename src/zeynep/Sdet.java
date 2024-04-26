a8b47c230567d64a18cda90bb34ef55d1df33b6c
zynpc <zynpckmk8@gmail.com>
2024-04-26 11:54:32 -0400

    created Sdet class in zeynep package

---
+++ src/zeynep/Sdet.java
@@ -0,0 +1,32 @@
+package zeynep;
+
+public class Sdet {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+/*
+Create a custom class named sdet with fallowing specifications:
+
+Attribites:
+
+            name (String)
+            age (int)
+            gender (char)
+            programmingLanguages (String)
+            isFullStackTester (boolean)
+
+Constructors:
+            Add multiple constructors to set different fields of the Sdets object.
+
+Actions:
+        work(): prints the  name of the employee concatenated with " is great Sdet"
+        toString(): returns a string representation of the sdet object.
+
+    Create another class named sdetClients, create multiple Sdets objects, and test each function of the sdet object.
+
+*/
+
