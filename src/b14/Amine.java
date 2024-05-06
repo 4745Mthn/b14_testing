8c1e4258e8e0b623feca25de772ed12df8705dd7
dilovar1992 <dellovar56@gmail.com>
2024-05-06 06:19:35 -0400

    created test message to everyone

--- src/b14/Amine.java
+++ src/b14/Amine.java
@@ -3,5 +3,6 @@ package b14;
 public class Amine {
     public static void main(String[] args) {
         System.out.println("I have the local and remote master");
+        System.out.println("this is test message");
     }
 }
--- src/zeynep/Cakmak.java
+++ src/zeynep/Cakmak.java
@@ -3,6 +3,6 @@ package zeynep;
 public class Cakmak {
 
     public static void main(String[] args) {
-
+        System.out.println("this is test message");
     }
 }
