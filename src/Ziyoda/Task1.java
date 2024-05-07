5c96d75fa99641c4dcab0e78bc593307476e9b9d
dilovar1992 <161163532+dilovar1992@users.noreply.github.com>
2024-05-07 14:16:24 -0400

    Update Ziyoda Task1.java

--- src/Ziyoda/Task1.java
+++ src/Ziyoda/Task1.java
@@ -1,4 +1,11 @@
 package Ziyoda;
 
 public class Task1 {
+  //write a method that returns prime number of the given N number
+//        prime number is th number that divisible only by 1 and itself
+//        parameter: int
+//        return type:void
+//        Ex: 5 is prime because it is divisible only with 1 and 5
+//9 is not prime because it is divisible by 1,3 and 9
+
 }
