82cd489b890bb1d088f2f37abcc61fae88453471
Amine Ersu <160289484+ersuamine@users.noreply.github.com>
2024-05-07 16:36:20 -0400

    More comments were added to PrimeNumbers.java

--- src/amine/PrimeNumbers.java
+++ src/amine/PrimeNumbers.java
@@ -3,9 +3,8 @@ package amine;
 public class PrimeNumbers {
     public static void main(String[] args) {
 
-        //for loop to iterates from 1 to 100 and check if it is prime\
+        //for loop to iterates from 1 to 100 and checks if it is prime\
         //and displays prime numbers
-
 
         for (int i = 1; i <= 100; i++) {
             if (isPrime(i)) {
@@ -18,23 +17,31 @@ public class PrimeNumbers {
         //check number and return true if it's prime
 
         //check if the number is even
+        // even numbers cannot be prime
+        //only even prime number is 2
         if (number != 2 && number % 2 == 0) {
-            return false;
+            return false; // not prime
         }
 
         // 2 is the only even prime number
         if (number ==2){
-            return true;
+            return true; // because it is prime
         }
         //check only odd numbers, even numbers cannot be prime, except #2
+
+        //factor is a number that you can divide another number without remainders
+        // 2 and 3 are factors of 6, and 6 can be divided by 2 and 3, giving a whole number result
+
         int factor = 0;
-        //prime numbers only have 1 and itself as factors
+        //prime numbers only have number 1 and itself as factors,
+        //example: factors of 2 is , 1 and 2
         for (int i = 1; i <= number; i += 2) {
-            if (number % i == 0) {
+            if (number % i == 0) {// there's no remainders, that means "i", is the factor of "number"
                 factor++;
             }
         }
         // factors are only 1 and given number itself, total 2
+        //example: factors of 2 is , 1 and 2, only 2 factors
         return factor == 2;
     }
 
