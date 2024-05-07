786a2c86f0a5bd0ef11d7e9d7e1bb92a7b0f2250
dilovar1992 <dellovar56@gmail.com>
2024-05-07 07:22:22 -0400

    prime numbers solved

--- src/dilovar/PrimeNumbers.java
+++ src/dilovar/PrimeNumbers.java
@@ -9,7 +9,14 @@ public class PrimeNumbers {
 public static void main(String[] args) {
     ArrayList<Integer> primes = new ArrayList<>();
     for (int i = 1; i < 101; i++) {
-        if (i%2!=0){
+        int count=0;
+        for (int j = 1; j <101 ; j++) {
+            if (i%j==0){
+                count++;
+            }
+
+        }
+        if (count==2){
             primes.add(i);
         }
 
