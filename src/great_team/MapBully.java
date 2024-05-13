19ca410a06b68fd72d427a26b7259a17e9dab86d
ersuamine <amine.ersu@gmail.com>
2024-05-13 14:06:56 -0400

    Added MapBully.java task

---
+++ src/great_team/MapBully.java
@@ -0,0 +1,19 @@
+package great_team;
+
+import java.util.Map;
+
+public class MapBully {
+    //commented out because of missing return statement since it's incomplete
+//    public Map<String, String> mapBully(Map<String, String> map) {
+//
+//    }
+}
+
+/*
+Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+
+
+mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
+ */
