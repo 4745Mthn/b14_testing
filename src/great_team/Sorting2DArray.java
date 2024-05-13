95a9182dfc2bc476e22703a992d6484faf02c90a
ersuamine <amine.ersu@gmail.com>
2024-05-13 14:02:49 -0400

    Added Sorting2DArray.java task

---
+++ src/great_team/Sorting2DArray.java
@@ -0,0 +1,12 @@
+package great_team;
+
+public class Sorting2DArray {
+    public static void main(String[] args) {
+        int[][] numbers = {{9, 6, 4, 2}, {6, 3, 4, 3, 54}, {0, 5, 3, 1, 11, 3}, {5, 4, 3, 6, 9, 8, 7, 2}};
+
+    }
+}
+/*
+Given a 2D array containing int arrays with different length, sort and display the 2D array
+Built in method are not allowed
+ */
