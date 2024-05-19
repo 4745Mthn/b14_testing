bfa632e1369a02becf5d113e0b7ae8e966788e84
ersuamine <amine.ersu@gmail.com>
2024-05-19 00:48:10 -0400

    Update ScanIntDoubleString.java with solution

--- src/great_team/ScanIntDoubleString.java
+++ src/great_team/ScanIntDoubleString.java
@@ -2,26 +2,38 @@ package great_team;
 
 import java.util.Scanner;
 
+
 public class ScanIntDoubleString {
+    /* Declare second integer, double, and String variables. */
+    /* Read and save an integer, double, and String to your variables.*/
+
+    /* Print the sum of both integer variables on a new line. */
+
+    /* Print the sum of the double variables on a new line. */
+
+    /* Concatenate and print the String variables on a new line;
+        	the 's' variable should be printed first. */
 
     public static void main(String[] args) {
+
         int i = 4;
         double d = 4.0;
-        String s = "HackerRank ";
-
+        String s = "GoldenFive ";
         Scanner scan = new Scanner(System.in);
 
-        /* Declare second integer, double, and String variables. */
+        int i2;
+        double d2;
+        String s2;
 
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        //nextline to catch the enter key pressed after the second number
+        scan.nextLine();
+        s2 = scan.nextLine();
 
-        /* Print the sum of both integer variables on a new line. */
-
-        /* Print the sum of the double variables on a new line. */
-
-        /* Concatenate and print the String variables on a new line;
-        	the 's' variable above should be printed first. */
+        System.out.println(i + i2);
+        System.out.println(d + d2);
+        System.out.println(s + s2);
 
         scan.close();
     }
