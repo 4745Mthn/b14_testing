31a8e913f0f567b98efc2d5fea168241f71aac5a
4745Mthn <metehan+metehan.ates@hotmail.com>
2024-05-21 15:32:21 -0400

    Updated table

--- src/great_team/MultiplicationTable.java
+++ src/great_team/MultiplicationTable.java
@@ -35,6 +35,20 @@ public class MultiplicationTable {
 
         bufferedReader.close();
     }
+
+
+        }
+class MultiplicationTable2 {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+
 }
 
 
