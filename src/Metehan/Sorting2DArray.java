84489f5fe45a4521695792e26e3ac5f6a291f33e
4745Mthn <metehan+metehan.ates@hotmail.com>
2024-05-21 15:08:37 -0400

    new changes

--- src/Metehan/Sorting2DArray.java
+++ src/Metehan/Sorting2DArray.java
@@ -1,24 +1,21 @@
-package great_team;
+package Metehan;
 
 import java.util.Arrays;
 
 public class Sorting2DArray {
-
-
     public static void main(String[] args) {
         int[][] numbers = {{9, 6, 4, 2}, {6, 3, 4, 3, 54}, {0, 5, 3, 1, 11, 3}, {5, 4, 3, 6, 9, 8, 7, 2}};
 
-        for (int i = 1; i < numbers.length; i++ ){
+        for (int i = 0; i < numbers.length; i++ ){
 
-            for ( int num = 0; num < numbers[0].length; num++){
+            for ( int num = 0; num < numbers[i].length-1; num++){
 
-                for (int j = i; j < numbers.length; j++)
-                    if (numbers[j - 1][num] > numbers[j][num]) {
+                for (int j = num +i; j < numbers.length; j++)
+                    if (numbers[i][num] > numbers[i][j]) {
 
-                        int temp = numbers[j][num];
-                        numbers[j][num] = numbers[j - 1][num];
-                        numbers[j - 1][num] = temp;
-
+                        int temp = numbers[i][j];
+                        numbers[i][j] = numbers[i][num];
+                        numbers[i][num] = temp;
 
                     }
             }
@@ -35,3 +32,4 @@ public class Sorting2DArray {
 Given a 2D array containing int arrays with different length, sort and display the 2D array
 Built in method are not allowed
  */
+
